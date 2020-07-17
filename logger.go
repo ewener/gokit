1b6c68b18096689116134011cf6113fc40be9a2a
daixw <daixw@ecpark.cn>
2020-07-17 18:26:35 +0800

    Revert "// update logger"
    This reverts commit 550aa733

--- logger.go
+++ logger.go
@@ -1,201 +1,140 @@
 package gokit
 
 import (
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	"sync"
+	"github.com/sirupsen/logrus"
+	"io"
 )
 
-var (
-	logLevel      = zap.NewAtomicLevelAt(zapcore.InfoLevel)
-	defaultLogger *ZapLogger
-	once          sync.Once
-)
+var globalLogger = logrus.New()
 
-type (
-	CustomLogger struct {
-		prefix string
-	}
-	ZapLogger struct {
-		underlieLogger *zap.SugaredLogger
-	}
-)
-
-func InitLogger(log *zap.Logger) {
-	once.Do(func() {
-		defaultLogger = &ZapLogger{underlieLogger: log.Sugar()}
-	})
+type CustomLogger struct {
+	Prefix string
+	*logrus.Logger
 }
 
-// new logger
+func InitLogger(formatter logrus.Formatter, hook logrus.Hook, output io.Writer) {
+	globalLogger.SetOutput(output)
+	globalLogger.SetFormatter(formatter)
+	globalLogger.AddHook(hook)
+}
+
 func NewCustomLogger(prefix string) *CustomLogger {
-	return &CustomLogger{prefix: "[" + prefix + "] "}
+	return &CustomLogger{Prefix: "[" + prefix + "] ", Logger: globalLogger}
 }
 
-func SetLevel(level string) {
-	var lvl zapcore.Level
-	lvl.Set(level)
-	logLevel.SetLevel(lvl)
+func (c *CustomLogger) SetPrefix(prefix string) {
+	c.Prefix = "[" + prefix + "] "
 }
 
-// print
-func (c *CustomLogger) Print(args ...interface{}) {
-	c.Info(args...)
+func (c *CustomLogger) Print(message ...interface{}) {
+	globalLogger.Print(c.Prefix, message)
 }
 
-// printf
-func (c *CustomLogger) Printf(format string, v ...interface{}) {
-	c.Infof(format, v)
+func (c *CustomLogger) Printf(format string, message ...interface{}) {
+	globalLogger.Printf(c.Prefix+format, message)
 }
 
-// Debug logger
-func (c *CustomLogger) Debug(args ...interface{}) {
-	defaultLogger.Debug(append([]interface{}{c.prefix}, args...)...)
+func (c *CustomLogger) Println(message ...interface{}) {
+	globalLogger.Println(c.Prefix, message)
 }
 
-// Info logger
-func (c *CustomLogger) Info(args ...interface{}) {
-	defaultLogger.Info(append([]interface{}{c.prefix}, args...)...)
+// Info level message
+func (c *CustomLogger) Info(message ...interface{}) {
+	globalLogger.Info(c.Prefix, message)
 }
 
-// Warn logger
-func (c *CustomLogger) Warn(args ...interface{}) {
-	defaultLogger.Warn(append([]interface{}{c.prefix}, args...)...)
+// Infof - formatted message
+func (c *CustomLogger) Infof(message string, args ...interface{}) {
+	globalLogger.Infof(c.Prefix+message, args...)
 }
 
-// Error logger
-func (c *CustomLogger) Error(args ...interface{}) {
-	defaultLogger.Error(append([]interface{}{c.prefix}, args...)...)
+// InfoFields - message with fields
+func (c *CustomLogger) InfoFields(message string, fields map[string]interface{}) {
+	globalLogger.WithFields(fields).Info(c.Prefix + message)
 }
 
-func (c *CustomLogger) Fatal(args ...interface{}) {
-	defaultLogger.Fatal(append([]interface{}{c.prefix}, args...)...)
+// Debug level message
+func (c *CustomLogger) Debug(message ...interface{}) {
+	globalLogger.Debug(c.Prefix, message)
 }
 
-func (c *CustomLogger) Debugf(format string, args ...interface{}) {
-	defaultLogger.Debugf(c.prefix+format, args...)
+// Debugf - formatted message
+func (c *CustomLogger) Debugf(message string, args ...interface{}) {
+	globalLogger.Debugf(c.Prefix+message, args...)
 }
 
-func (c *CustomLogger) Infof(format string, args ...interface{}) {
-	defaultLogger.Infof(c.prefix+format, args...)
+// DebugFields - message with fields
+func (c *CustomLogger) DebugFields(message string, fields map[string]interface{}) {
+	globalLogger.WithFields(fields).Debug(c.Prefix + message)
 }
 
-func (c *CustomLogger) Warnf(format string, args ...interface{}) {
-	defaultLogger.Warnf(c.prefix+format, args...)
+// Warn level message
+func (c *CustomLogger) Warn(message string) {
+	globalLogger.Warn(c.Prefix + message)
 }
 
-func (c *CustomLogger) Errorf(format string, args ...interface{}) {
-	defaultLogger.Errorf(c.prefix+format, args...)
+// Warnf - formatted message
+func (c *CustomLogger) Warnf(message string, args ...interface{}) {
+	globalLogger.Warnf(c.Prefix+message, args...)
 }
 
-func (c *CustomLogger) Fatalf(format string, args ...interface{}) {
-	defaultLogger.Fatalf(c.prefix+format, args...)
+// WarnFields - message with fields
+func (c *CustomLogger) WarnFields(message string, fields map[string]interface{}) {
+	globalLogger.WithFields(fields).Warn(c.Prefix + message)
 }
 
-func (c *CustomLogger) Panicf(format string, args ...interface{}) {
-	defaultLogger.Panicf(c.prefix+format, args...)
+// Error level message
+func (c *CustomLogger) Error(message ...interface{}) {
+	globalLogger.Error(c.Prefix, message)
 }
 
-func (c *CustomLogger) DebugFields(msg string, fields ...zapcore.Field) {
-	defaultLogger.DebugWithFields(c.prefix+msg, fields...)
+// Errorf - formatted message
+func (c *CustomLogger) Errorf(message string, args ...interface{}) {
+	globalLogger.Errorf(c.Prefix+message, args...)
 }
 
-func (c *CustomLogger) InfoFields(msg string, fields ...zapcore.Field) {
-	defaultLogger.InfoWithFields(c.prefix+msg, fields...)
+// ErrorFields - message with fields
+func (c *CustomLogger) ErrorFields(message string, fields map[string]interface{}) {
+	globalLogger.WithFields(fields).Error(c.Prefix + message)
 }
 
-func (c *CustomLogger) WarnFields(msg string, fields ...zapcore.Field) {
-	defaultLogger.WarnWithFields(c.prefix+msg, fields...)
+// Fatal level message
+func (c *CustomLogger) Fatal(message string) {
+	globalLogger.Fatal(c.Prefix + message)
 }
 
-func (c *CustomLogger) ErrorFields(msg string, fields ...zapcore.Field) {
-	defaultLogger.ErrorWithFields(c.prefix+msg, fields...)
+// Fatalf - formatted message
+func (c *CustomLogger) Fatalf(message string, args ...interface{}) {
+	globalLogger.Fatalf(c.Prefix+message, args...)
 }
 
-func (c *CustomLogger) PanicFields(msg string, fields ...zapcore.Field) {
-	defaultLogger.PanicWithFields(c.prefix+msg, fields...)
+// FatalFields - message with fields
+func (c *CustomLogger) FatalFields(message string, fields map[string]interface{}) {
+	globalLogger.WithFields(fields).Fatal(c.Prefix + message)
 }
 
-func (c *CustomLogger) FatalFields(msg string, fields ...zapcore.Field) {
-	defaultLogger.FatalWithFields(c.prefix+msg, fields...)
+// Panic level message
+func (c *CustomLogger) Panic(message string) {
+	globalLogger.Panic(c.Prefix + message)
 }
 
-// ///////////////////////////////////////////////////
-
-// Debug ZapLogger
-func (l *ZapLogger) Debug(args ...interface{}) {
-	l.underlieLogger.Debug(args...)
+// Panicf - formatted message
+func (c *CustomLogger) Panicf(message string, args ...interface{}) {
+	globalLogger.Panicf(c.Prefix+message, args...)
 }
 
-// Info logger
-func (l *ZapLogger) Info(args ...interface{}) {
-	l.underlieLogger.Info(args...)
+// PanicFields - message with fields
+func (c *CustomLogger) PanicFields(message string, fields map[string]interface{}) {
+	globalLogger.WithFields(fields).Panic(c.Prefix + message)
 }
 
-// Warn logger
-func (l *ZapLogger) Warn(args ...interface{}) {
-	l.underlieLogger.Warn(args...)
-}
-
-// Error logger
-func (l *ZapLogger) Error(args ...interface{}) {
-	l.underlieLogger.Error(args...)
-}
-
-func (l *ZapLogger) Fatal(args ...interface{}) {
-	l.underlieLogger.Fatal(args...)
-}
-
-func (l *ZapLogger) Debugf(format string, args ...interface{}) {
-	l.underlieLogger.Debugf(format, args...)
-}
-
-func (l *ZapLogger) Infof(format string, args ...interface{}) {
-	l.underlieLogger.Infof(format, args...)
-}
-
-func (l *ZapLogger) Warnf(format string, args ...interface{}) {
-	l.underlieLogger.Warnf(format, args...)
-}
-
-func (l *ZapLogger) Errorf(format string, args ...interface{}) {
-	l.underlieLogger.Errorf(format, args...)
-}
-
-func (l *ZapLogger) Fatalf(format string, args ...interface{}) {
-	l.underlieLogger.Fatalf(format, args...)
-}
-
-func (l *ZapLogger) Panicf(format string, args ...interface{}) {
-	l.underlieLogger.Panicf(format, args...)
-}
-
-func (l *ZapLogger) DebugWithFields(msg string, fields ...zap.Field) {
-	logger := l.underlieLogger.Desugar()
-	logger.Debug(msg, fields...)
-}
-
-func (l *ZapLogger) InfoWithFields(msg string, fields ...zap.Field) {
-	logger := l.underlieLogger.Desugar()
-	logger.Info(msg, fields...)
-}
-
-func (l *ZapLogger) WarnWithFields(msg string, fields ...zap.Field) {
-	logger := l.underlieLogger.Desugar()
-	logger.Warn(msg, fields...)
-}
-
-func (l *ZapLogger) ErrorWithFields(msg string, fields ...zap.Field) {
-	logger := l.underlieLogger.Desugar()
-	logger.Error(msg, fields...)
-}
-
-func (l *ZapLogger) PanicWithFields(msg string, fields ...zap.Field) {
-	logger := l.underlieLogger.Desugar()
-	logger.Panic(msg, fields...)
-}
-
-func (l *ZapLogger) FatalWithFields(msg string, fields ...zap.Field) {
-	logger := l.underlieLogger.Desugar()
-	logger.Fatal(msg, fields...)
+func (c *CustomLogger) SetLevel(level string) error {
+	var (
+		err      error
+		logLevel logrus.Level
+	)
+	if logLevel, err = logrus.ParseLevel(level); err == nil {
+		globalLogger.SetLevel(logLevel)
+	}
+	return err
 }
