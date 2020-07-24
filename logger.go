a88ed98f6908d0411e6796a3d16c3f5fd9cb6df2
daixw <daixw@ecpark.cn>
2020-07-24 11:24:28 +0800

    //fix logger

--- logger.go
+++ logger.go
@@ -12,11 +12,6 @@ var (
 type (
 	CustomLogger struct {
 		Prefix string
-		*ZapLogger
-	}
-	ZapLogger struct {
-		logger      *zap.Logger
-		sugarLogger *zap.SugaredLogger
 	}
 )
 
@@ -28,89 +23,87 @@ func InitLogger(log *zap.Logger) {
 
 // new logger
 func NewCustomLogger(prefix string) *CustomLogger {
-	return &CustomLogger{Prefix: "[" + prefix + "] ", ZapLogger: &ZapLogger{
-		logger: zap.L(), sugarLogger: zap.S(),
-	}}
+	return &CustomLogger{Prefix: "[" + prefix + "] "}
 }
 
 // print
 func (c *CustomLogger) Print(args ...interface{}) {
-	c.Info(args...)
+	zap.S().Info(args...)
 }
 
 // printf
 func (c *CustomLogger) Printf(format string, v ...interface{}) {
-	c.Infof(format, v)
+	zap.S().Infof(format, v)
 }
 
 // Debug ZapLogger
-func (l *ZapLogger) Debug(args ...interface{}) {
-	l.sugarLogger.Debug(args...)
+func (c *CustomLogger) Debug(args ...interface{}) {
+	zap.S().Debug(args...)
 }
 
 // Info logger
-func (l *ZapLogger) Info(args ...interface{}) {
-	l.sugarLogger.Info(args...)
+func (c *CustomLogger) Info(args ...interface{}) {
+	zap.S().Info(args...)
 }
 
 // Warn logger
-func (l *ZapLogger) Warn(args ...interface{}) {
-	l.sugarLogger.Warn(args...)
+func (c *CustomLogger) Warn(args ...interface{}) {
+	zap.S().Warn(args...)
 }
 
 // Error logger
-func (l *ZapLogger) Error(args ...interface{}) {
-	l.sugarLogger.Error(args...)
+func (c *CustomLogger) Error(args ...interface{}) {
+	zap.S().Error(args...)
 }
 
-func (l *ZapLogger) Fatal(args ...interface{}) {
-	l.sugarLogger.Fatal(args...)
+func (c *CustomLogger) Fatal(args ...interface{}) {
+	zap.S().Fatal(args...)
 }
 
-func (l *ZapLogger) Debugf(format string, args ...interface{}) {
-	l.sugarLogger.Debugf(format, args...)
+func (c *CustomLogger) Debugf(format string, args ...interface{}) {
+	zap.S().Debugf(format, args...)
 }
 
-func (l *ZapLogger) Infof(format string, args ...interface{}) {
-	l.sugarLogger.Infof(format, args...)
+func (c *CustomLogger) Infof(format string, args ...interface{}) {
+	zap.S().Infof(format, args...)
 }
 
-func (l *ZapLogger) Warnf(format string, args ...interface{}) {
-	l.sugarLogger.Warnf(format, args...)
+func (c *CustomLogger) Warnf(format string, args ...interface{}) {
+	zap.S().Warnf(format, args...)
 }
 
-func (l *ZapLogger) Errorf(format string, args ...interface{}) {
-	l.sugarLogger.Errorf(format, args...)
+func (c *CustomLogger) Errorf(format string, args ...interface{}) {
+	zap.S().Errorf(format, args...)
 }
 
-func (l *ZapLogger) Fatalf(format string, args ...interface{}) {
-	l.sugarLogger.Fatalf(format, args...)
+func (c *CustomLogger) Fatalf(format string, args ...interface{}) {
+	zap.S().Fatalf(format, args...)
 }
 
-func (l *ZapLogger) Panicf(format string, args ...interface{}) {
-	l.sugarLogger.Panicf(format, args...)
+func (c *CustomLogger) Panicf(format string, args ...interface{}) {
+	zap.S().Panicf(format, args...)
 }
 
-func (l *ZapLogger) DebugFields(msg string, fields ...zap.Field) {
-	l.logger.Debug(msg, fields...)
+func (c *CustomLogger) DebugFields(msg string, fields ...zap.Field) {
+	zap.L().Debug(msg, fields...)
 }
 
-func (l *ZapLogger) InfoFields(msg string, fields ...zap.Field) {
-	l.logger.Info(msg, fields...)
+func (c *CustomLogger) InfoFields(msg string, fields ...zap.Field) {
+	zap.L().Info(msg, fields...)
 }
 
-func (l *ZapLogger) WarnFields(msg string, fields ...zap.Field) {
-	l.logger.Warn(msg, fields...)
+func (c *CustomLogger) WarnFields(msg string, fields ...zap.Field) {
+	zap.L().Warn(msg, fields...)
 }
 
-func (l *ZapLogger) ErrorFields(msg string, fields ...zap.Field) {
-	l.logger.Error(msg, fields...)
+func (c *CustomLogger) ErrorFields(msg string, fields ...zap.Field) {
+	zap.L().Error(msg, fields...)
 }
 
-func (l *ZapLogger) PanicFields(msg string, fields ...zap.Field) {
-	l.logger.Panic(msg, fields...)
+func (c *CustomLogger) PanicFields(msg string, fields ...zap.Field) {
+	zap.L().Panic(msg, fields...)
 }
 
-func (l *ZapLogger) FatalFields(msg string, fields ...zap.Field) {
-	l.logger.Fatal(msg, fields...)
+func (c *CustomLogger) FatalFields(msg string, fields ...zap.Field) {
+	zap.L().Fatal(msg, fields...)
 }
