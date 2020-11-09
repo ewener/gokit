b2dfbc7228438fff27e1bfd4e869d4663965c2cb
daixw <daixw@ecpark.cn>
2020-11-09 16:27:51 +0800

    go safety

--- goroutine.go
+++ goroutine.go
@@ -10,17 +10,38 @@ import (
 )
 
 // GoSafely wraps a `go func()` with recover()
-func GoSafely(wg *sync.WaitGroup, handler func(), ignoreRecover bool) {
+func GoSafely(wg *sync.WaitGroup, ignoreRecover bool, handler func(), catchFunc func(r interface{})) {
 	if wg != nil {
 		wg.Add(1)
 	}
 	go func() {
 		defer func() {
 			if r := recover(); r != nil {
-				if !ignoreRecover {
+				if ignoreRecover {
 					panicLog(fmt.Sprintf("goroutine panic: %v\n%s\n",
 						r, string(debug.Stack())))
-					os.Exit(-1)
+					os.Exit(1)
+				}
+				if catchFunc != nil {
+					if wg != nil {
+						wg.Add(1)
+					}
+					go func() {
+						defer func() {
+							if p := recover(); p != nil {
+								if ignoreRecover {
+									panicLog(fmt.Sprintf("recover goroutine panic:%v\n%s\n", p, string(debug.Stack())))
+									os.Exit(1)
+								}
+
+							}
+
+							if wg != nil {
+								wg.Done()
+							}
+						}()
+						catchFunc(r)
+					}()
 				}
 			}
 			if wg != nil {
@@ -37,6 +58,7 @@ func panicLog(info string) {
 		return
 	}
 	now := time.Now()
+	// todo 按日期分文件，避免过大
 	logPath := filepath.Join(dir, "crash.log")
 	file, er := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
 	if er == nil {
